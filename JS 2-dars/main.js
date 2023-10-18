1f6552f288ea857c8edd1fc5724f7ce8aa55c165
Islom Turgunov <147254633+QochqorovBaxtiyor@users.noreply.github.com>
2023-10-18 17:27:31 +0500

    JS 2-dars
    27-guruh

---
+++ JS 2-dars/main.js
@@ -0,0 +1,82 @@
+
+
+
+/*
+if ("salom" == "salom"){
+    console.log("Ular teng!")
+}
+else {
+    console.log("Ular teng emas!")
+}*/
+
+
+
+/*if (45 == 45){
+    console.log("Ular teng!-1")
+}
+else if (45 == 45){
+    console.log("Ular teng!-2")
+}
+else{
+    console.log("Yuqoridagilarning hech biri teng emas!")
+}*/
+
+/*
+if ("FrontEnd"){
+    console.log("Ular teng!-1")
+}
+else if ("FrontEnd"){
+    console.log("Ular teng!-2")
+}
+else{
+    console.log("Notog'ri!")
+}
+
+if (123 == 123){
+    console.log("Ular teng!")
+}
+else if (789 == 908){
+    console.log("Ular teng!")
+}
+else{
+    console.log("Ular teng emas!")
+}
+if ("It organish yaxshimi!"){
+    console.log("Xa albattta!")
+}
+else if ("It organish yaxshimi!"){
+    console.log("Xa albatta!")
+}
+else {
+    console.log("Togri javob!")
+}*/
+
+
+let a = 10
+let b = 46
+let c = 49
+let d = 55
+
+/*if (a == 89 || b == 78 || c == 48 || d == 57){
+    console.log("Qiymatlarning kamida bittasi  tog'ri!");
+}
+else{
+    console.log(" Qiymatlarning hammasi notog'ri  hato !")
+}
+if (a == 10 || b == 46 || c == 49 || d == 55){
+    console.log("Solidhtirilggan qiymatlarning barchasi tog'ri")
+}
+else{
+
+}*/
+
+
+/*
+let  matn = "Java Script";
+let son = 45
+
+document.write(typeof son)*/
+
+let rost = 45 == 45 ? true:false;
+
+document.write(rost)
